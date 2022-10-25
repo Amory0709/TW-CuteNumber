28f08506c46997654919537d6bd8f40489208e0c
Mengyu <mhan8@slb.com>
2022-10-25 13:42:21 +0800

    feat: first TDD code

--- src/Foo.ts
+++ src/Foo.ts
@@ -3,3 +3,27 @@ export class Foo {
     return 'bar'
   }
 }
+
+
+export function isCuteNumberGreaterThan10(numbers: number[]): boolean{
+  const evenNumbers = findAllEvenNumbers(numbers)
+  const maxNum = findTheMax(evenNumbers)
+  if (!maxNum) {
+    return false
+  }
+  return isGreaterThan10(maxNum)
+}
+
+function findAllEvenNumbers(numbers: number[]){
+  return numbers.filter( num => num % 2 === 0)
+}
+
+function findTheMax(numbers: number[]){
+  return Math.max(...numbers)
+}
+
+function isGreaterThan10(num: number){
+  return num > 10
+}
+
+
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,25 @@
-import {Foo} from '../src/Foo'
+import {Foo, isCuteNumberGreaterThan10} from '../src/Foo'
 
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
+  })
+
+  it('should return true when given array: 2, 4, 7, 9, 11, 14', () => {
+    const numbers = [2, 4, 7, 9, 11, 14]
+    const res = isCuteNumberGreaterThan10(numbers)
+    expect(res).toBeTruthy()
+  })
+
+  it('should return false when given array: 2, 4, 6, 9', () => {
+    const numbers = [2, 4, 6, 9]
+    const res = isCuteNumberGreaterThan10(numbers)
+    expect(res).toBeFalsy()
+  })
+
+  it('should return false when given array: 2, 4, 6', () => {
+    const numbers = [2, 4, 6]
+    const res = isCuteNumberGreaterThan10(numbers)
+    expect(res).toBeFalsy()
   })
 })
